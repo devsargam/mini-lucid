42991c6930e67be0fad2680bde66a06f782c6108
Sargam Poudel <sargampoudel100@gmail.com>
2025-04-22 22:03:07 +0530

    add sidebar

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,12 @@
+import { AppSidebar } from "@/components/app-sidebar";
+
+export default function EditorPage() {
+  return (
+    <div>
+      {/* Left Sidebar */}
+      <AppSidebar />
+      {/* Main Content */}
+      {/* Right Sidebar Resizable */}
+    </div>
+  );
+}
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,75 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  Calendar,
+  GalleryVerticalEnd,
+  Home,
+  Inbox,
+  LucidePenLine,
+  PenLine,
+  PenLineIcon,
+  Search,
+  Settings,
+} from "lucide-react";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <LucidePenLine className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Mini Lucid</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
