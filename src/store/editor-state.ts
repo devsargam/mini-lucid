1bffbcbc145b339fcc38bdfcea7735ee74fe96e3
Sargam Poudel <sargampoudel100@gmail.com>
2025-04-23 02:21:11 +0530

    change copy

--- src/store/editor-state.ts
+++ src/store/editor-state.ts
@@ -7,6 +7,6 @@ interface EditorState {
 }
 
 export const useEditorState = create<EditorState>((set) => ({
-  value: null,
+  value: `<p class="text-node">Hope you liked my efforts and the product I built please provide me feedback here.</p><p class="text-node"><span><strong>Thank you</strong></span> for taking the time to share your thoughts!</p><p class="text-node"><em>Looking forward to hearing from you soon.</em></p><p class="text-node">`,
   setValue: (value: Content) => set({ value }),
 }));
